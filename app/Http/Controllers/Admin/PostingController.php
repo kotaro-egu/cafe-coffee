2db3ac5b59515a41d4079f6b654d25608aa2a2aa
EC2 Default User <ec2-user@ip-172-31-8-211.us-east-2.compute.internal>
2021-10-12 10:59:54 +0000

    cafe-coffee

--- app/Http/Controllers/Admin/PostingController.php
+++ app/Http/Controllers/Admin/PostingController.php
@@ -5,17 +5,38 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Posting;
+
 class PostingController extends Controller
 {
-    //
+
   public function add()
   {
     return view('admin.posting.create');
   }
 
-  public function create(Request $request)
+ public function create(Request $request)
   {
-    return redirect('admin/news/create');
-  }  
-}
+      // このファンクションを実行することによってデータベースのnewsテーブルにデータが追加される
+      $this->validate($request, Posting::$rules);
+      $post = new Posting;
+      $form = $request->all();
+      if (isset($form['image'])) {
+          $path = $request->file('image')->store('public/image');
+          //$path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+          $post->image_path = Storage::disk('s3')->url($path);
+      } else {
+          $post->image_path = null;
+      }
 
+      unset($form['_token']);
+
+      unset($form['image']);
+
+      $post->fill($form);
+      $oost->save();
+
+      return redirect('admin/posting/create');
+  }
+
+}
---
+++ app/Posting.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Posting extends Model
+{
+    protected $guarded = array('id');
+    //
+    public static $rules = array(
+        'title' => 'required',
+        'body'=> 'required',
+        'image_path'=>'required',
+    );
+}
+
+
