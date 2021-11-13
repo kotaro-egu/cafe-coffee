44f401ea406e5b6f87b10b3a8802b55ac907e61b
EC2 Default User <ec2-user@ip-172-31-8-211.us-east-2.compute.internal>
2021-11-13 09:55:06 +0000

    余分空白修正

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -20,7 +20,7 @@
         <script src="{{ secure_asset('js/app.js') }}" defer></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="{{ secure_asset('js/topreverse.js') }}" defer></script>
-    　　
+        
         <!-- Fonts -->
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
